32c8ac5f806d5341a0bf69931e904deaeb03e496
Antony Reyes <antony.reyespilo@gmail.com>
2025-03-26 21:53:29 -0500

    Added database connection and API files

--- config/database.php
+++ config/database.php
@@ -1,25 +1,26 @@
 <?php
 class Database {
-    private $host = "dpg-cvhm5l1opnds73fl4170-a";  // Change to Render or Localhost
+    private $host = "dpg-cvhm5l1opnds73fl4170-a";  // Change if needed
     private $port = '5432';
     private $db_name = "inf653_ar_midterm_7e6a";
     private $username = "inf653_ar_midterm_7e6a_user";
     private $password = "5zCP7GMDvHwtrpXF9D0hpgWFr6TGpQSu";
-    public $conn;
-}
+    private $conn;
 
-public function _construct() {
-    if ($this->conn) {
-        return $this->conn;
-    } else {
-        $dsn = "pgsl:host = {$this->host};port={$this->dbname};";
+    public function __construct() {
+        $this->conn = null;
+        $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->db_name};";
+
         try {
             $this->conn = new PDO($dsn, $this->username, $this->password);
-            $this->conn->setAttribute(PDO::ATTR-ERRMODE, PDO::ERRMODE_EXCEPTION);
-            return $this->conn;
-        } catch(PDOException $e){
-            echo 'Connection Error: ' . $e->getMessage();
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Connection Error: " . $e->getMessage());
         }
+    }
+
+    public function connect() {
+        return $this->conn;
     }
 }
 ?>
