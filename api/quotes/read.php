9daf2e02de07f7b0b54e2710890e9d9de52953ea
Antony Reyes <antony.reyespilo@gmail.com>
2025-03-26 20:30:14 -0500

    Added database connection and API files

---
+++ api/quotes/read.php
@@ -0,0 +1,32 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+include_once '../../config/database.php';
+include_once '../../models/Quote.php';
+
+// Initialize DB connection
+$database = new Database();
+$db = $database->connect();
+
+$quote = new Quote($db);
+$result = $quote->read();
+
+if ($result->rowCount() > 0) {
+    $quotes_arr = array("data" => array());
+
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        extract($row);
+        $quotes_arr["data"][] = array(
+            "id" => $id,
+            "quote" => $quote,
+            "author" => $author_name,
+            "category" => $category_name
+        );
+    }
+
+    echo json_encode($quotes_arr);
+} else {
+    echo json_encode(array("message" => "No quotes found."));
+}
+?>
